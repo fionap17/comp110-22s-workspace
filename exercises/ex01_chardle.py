dafe930bf821f646d51db0d2414a85b04333b36e
Fiona Pu <fionap17@live.unc.edu>
2022-01-20 09:21:12 -0500

    worked on ex01

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,7 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730243735"
+
+chosen_word: str = input("Enter a 5-character word:")
+chosen_letter: str = input("Enter a single character:")
+print("Searching for " + chosen_letter + " in " + chosen_word + "")
