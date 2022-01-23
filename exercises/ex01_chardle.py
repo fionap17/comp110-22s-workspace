f78076f7f2d8af21d5e0910f5f59fbd2cafb51e7
Fiona Pu <fionap17@live.unc.edu>
2022-01-23 17:34:38 -0500

    completed ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,14 +2,12 @@
 
 __author__ = "730243735"
 
-HELLO: str = str("hello")
-HEELS: str = str("heels")
-LETTER_E: str = str("e")
-LETTER_H: str = str("h")
-
 chosen_word: str = input("Enter a 5-character word:")
 if len(chosen_word) != 5:
     print("Error: word must contain 5 characters")
+    exit()
+if chosen_word.isalpha() != "hello".isalpha():
+    print("Error word must contain only characters of the alphabet")
     exit()
 else:
     chosen_letter: str = input("Enter a single character:")
@@ -17,19 +15,39 @@ else:
         print("Error: Character must be a single character.")
         exit()
     else:
-        print("Searching for " + chosen_letter + " in " + chosen_word + "")
-        if chosen_letter != str("h") or str("e") or str("l") or str("o") or str("s"):
-            if chosen_word == HELLO:
-                if chosen_letter == LETTER_E:
-                    print("e found at index 1")
-                    print("1 instance of e found in hello")
-            if chosen_word == HEELS:
-                if chosen_letter == LETTER_E:
-                    print("e found at index 1")
-                    print("e found at index 2")
-                    print("2 instances found in heels")
-                if chosen_letter == LETTER_H:
-                    print("h found at index 0")
-                    print("1 instance of h found in heels")
+        print("Searching for " + chosen_letter + " in " + chosen_word)
+        char1: str = chosen_word[0]
+        char2: str = chosen_word[1]
+        char3: str = chosen_word[2]
+        char4: str = chosen_word[3]
+        char5: str = chosen_word[4]
+        count1: int = 0
+        count2: int = 0
+        count3: int = 0
+        count4: int = 0
+        count5: int = 0
+        if char1 == chosen_letter:
+            print(chosen_letter + " found at index 0")
+            count1 = 1
+        if char2 == chosen_letter:
+            print(chosen_letter + " found at index 1")
+            count2 = 1
+        if char3 == chosen_letter:
+            print(chosen_letter + " found at index 2")
+            count3 = 1
+        if char4 == chosen_letter:
+            print(chosen_letter + " found at index 3")
+            count4 = 1
+        if char5 == chosen_letter:
+            print(chosen_letter + " found at index 4")
+            count5 = 1
+        if count1 + count2 + count3 + count4 + count5 != 0: 
+            print(str(count1 + count2 + count3 + count4 + count5) + " instances of " + chosen_letter + " found in " + chosen_word)
         else:
-            print("No instances of " + chosen_letter + " found in " + chosen_word + "")
+            count1: int = 0
+            count2: int = 0
+            count3: int = 0
+            count4: int = 0
+            count5: int = 0
+            print("No instances of " + chosen_letter + " found in " + chosen_word)
+            exit()
