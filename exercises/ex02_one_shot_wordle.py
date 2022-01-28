7f9c803ad9c98d699095ef6fa0fb33b7f7b9e2a0
Fiona Pu <fionap17@live.unc.edu>
2022-01-28 17:09:19 -0500

    completed part 3

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,18 +7,27 @@ guess: str = str(input("what is your 6-letter guess?"))
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-index: int = 0
+idx: int = 0
 s: str = str("")
 
 while len(guess) < len(SECRET_WORD) or len(guess) > len(SECRET_WORD):
     guess: str = str(input("That was not 6 letters. try again:"))
 else:
-    while index < len(SECRET_WORD):
-        if guess[index] == SECRET_WORD[index]:
+    while idx < len(SECRET_WORD):
+        if guess[idx] == SECRET_WORD[idx]:
             s = s + GREEN_BOX
         else:
-            s = s + WHITE_BOX
-        index = index + 1
+            var: bool = False
+            alt_idx: int = 0
+            while not var and alt_idx < len(SECRET_WORD):
+                if guess[idx] == SECRET_WORD[alt_idx]:
+                    var: bool = True
+                alt_idx = alt_idx + 1
+            if var:
+                s = s + YELLOW_BOX
+            else:
+                s = s + WHITE_BOX
+        idx = idx + 1
 print(s)
 if guess == SECRET_WORD:
     print("Woo! You got it!")
